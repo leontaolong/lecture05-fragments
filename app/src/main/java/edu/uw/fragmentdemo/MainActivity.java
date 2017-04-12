f4cb957a40ca2cb65751ed1bef6d5de3a7ed4fd7
leontaolong <longt8@uw.edu>
2017-04-11 20:13:20 -0700

    Complete the lab work

--- app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
+++ app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
@@ -1,20 +1,39 @@
 package edu.uw.fragmentdemo;
 
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements MoviesFragment.OnMovieSelectedListener {
+public class MainActivity extends AppCompatActivity implements MoviesFragment.OnMovieSelectedListener, SearchFragment.OnSearchListener {
 
     private static final String TAG = "MainActivity";
+
+    private SearchFragment searchFragment;
+    private MoviesFragment moviesFragment;
+    private DetailFragment detailFragment;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        searchFragment = SearchFragment.newInstance();
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new MoviePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
     }
 
 
@@ -31,6 +50,14 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
         ft.commit();
     }
 
+
+    @Override
+    public void onSearchSubmitted(String searchTerm) {
+        moviesFragment = MoviesFragment.newInstance(searchTerm);
+        pagerAdapter.notifyDataSetChanged();
+        viewPager.setCurrentItem(1); //hard-code the shift
+    }
+
     @Override
     public void onMovieSelected(Movie movie) {
         DetailFragment detailFragment = DetailFragment.newInstance(movie.toString(), movie.imdbId);
@@ -39,5 +66,49 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
                 .replace(R.id.container, detailFragment, null)
                 .addToBackStack(null)
                 .commit();
+    }
+
+    private class MoviePagerAdapter extends FragmentStatePagerAdapter {
+
+        public MoviePagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            //Hard-code the ordering as an example
+            if(position == 0) return searchFragment;
+            if(position == 1) return moviesFragment;
+            if(position == 2) return detailFragment;
+            return null; //just in case
+        }
+
+        //work-around for Fragment replacement
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+
+        @Override
+        public int getCount() {
+            if(moviesFragment == null){
+                return 1;
+            } else if (detailFragment == null) {
+                return 2;
+            } else {
+                return 3;
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
     }
 }
